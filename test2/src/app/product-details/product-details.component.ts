ed34ccdfacae95d47b7a78f7bf0843a8962526c6
OinkoIT <claus.mersch@gmail.com>
2022-06-15 13:00:43 +0200

    add to cart button

--- test2/src/app/product-details/product-details.component.ts
+++ test2/src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { products } from '../products';
 import { IProducts } from '../IProducts';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,12 @@ export class ProductDetailsComponent implements OnInit {
   product: IProducts = {} as IProducts;
   id: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+
+  addToCart() {
+    window.alert("Your Pizza has been added to the cart!")
+    this.cartService.addToCart(this.product)
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
