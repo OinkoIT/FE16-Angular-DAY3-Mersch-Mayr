d52a1a8f005954173b94c587261e0544d5507539
OinkoIT <claus.mersch@gmail.com>
2022-06-15 13:18:14 +0200

    link checkout button

--- test2/src/app/app-routing.module.ts
+++ test2/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
@@ -8,6 +9,8 @@ const routes: Routes = [
     path:"", component: ProductListComponent
   },{
     path:"products/:productId", component: ProductDetailsComponent
+  },{
+    path:"cart", component: CartComponent
   }
 ];
 
--- test2/src/app/app.module.ts
+++ test2/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     NavbarComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
